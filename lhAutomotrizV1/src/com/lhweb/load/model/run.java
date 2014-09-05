7fe423e2efc6040674521a92e2782c538b11aec2
javo <javo@hmjavier>
2014-09-05 05:01:41 -0500

    images

--- lhAutomotrizV1/src/com/lhweb/load/model/run.java
+++ lhAutomotrizV1/src/com/lhweb/load/model/run.java
@@ -8,9 +8,9 @@ public class run {
 	public static void main(String[] args) {
 		
 		
-		Automaker automaker = new Automaker("Volkswagen", Arrays.asList("Gol", "Passat"));
-		//automaker.create();
-		AutomakerController.getAll();
+		Automaker automaker = new Automaker("1","Volkswagen", Arrays.asList("Gol", "Passat"));
+		automaker.create();
+		//AutomakerController.getAll();
 		
 
 	
